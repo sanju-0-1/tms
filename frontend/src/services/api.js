513b2dade24e56b61e4d3ecbe4873e839f7309cf
sanju-0-1 <fardheensanju@gmail.com>
2026-03-03 23:46:41 +0530

    Fix syntax error in api.js causing build failure

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
 
 const getToken = () => {

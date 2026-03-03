3e14c3741ced76c91f3cdeecd3217c36ae1ad264
Fardheen M <fardheensanju@gmail.com>
2026-03-03 13:44:30 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
 
 const getToken = () => {
   return localStorage.getItem("token");

01c4e94e7bed387b5e0a735c46f46a7fb1be6065
Fardheen M <fardheensanju@gmail.com>
2026-03-03 13:05:35 +0530

    Update API base URL to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://tms-047i.onrender.com";
 
 const getToken = () => {
   return localStorage.getItem("token");
